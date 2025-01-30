145d96f61bdee316f36da583ebb4ea41d054414c
hannes427 <hannes.rusch@web.de>
2025-01-30 09:14:04 +0100

    update power.js

--- assets/js/views/power.js
+++ assets/js/views/power.js
@@ -17,7 +17,7 @@ $(document)
     var req = {
 			module: 'systemadmin',
 			command: 'powermgmt',
-			action: 'reboot',
+			action: 'poweroff',
 		};
 
         $.ajax({
@@ -36,6 +36,41 @@ $(document)
 }
 });
 }});
+
+
+
+
+
+
+$(document)
+.on('click', '#reboot', function(e) {
+    if (!confirm('Are you shure to reboot the system? Active calls will be terminated and no new calls will be processed during reboot!')) {
+        e.preventDefault(); //stop submit
+    }
+ else {
+    var req = {
+			module: 'systemadmin',
+			command: 'powermgmt',
+			action: 'reboot',
+		};
+        $.ajax({
+            type: "POST",
+			url: FreePBX.ajaxurl,
+			data: req,
+			success: function(data) {
+}
+});
+}});
+
+
+
+
+
+
+
+
+
+
 /*$(document)
 .on('click', '#reboot', function(e) {
     if (!confirm('Are you shure to reboot the system? Active calls will be terminated and no new calls will be processed during reboot!')) {
